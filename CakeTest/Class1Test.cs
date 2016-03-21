87e48a0a71b18c634dea8702653b4b031841879a
Peuch <lucas.78.13@gmail.com>
2016-03-21 15:10:24 +0100

    Initial commit

---
+++ CakeTest/Class1Test.cs
@@ -0,0 +1,15 @@
+﻿using Cake;
+using NUnit.Framework;
+
+namespace CakeTest
+{
+    public class Class1Test
+    {
+        [Test]
+        public void testAdd()
+        {
+            Class1 class1 = new Class1();
+            Assert.AreEqual(3, class1.add(1, 2));
+        }
+    }
+}
